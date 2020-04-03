7e8fa65348e555418681251f5ee2751f6eee3861
JoakimWulf <60509179+JoakimWulf@users.noreply.github.com>
2020-04-03 12:16:08 +0200

    Update showSESDemo.js
    Update

--- showSESDemo.js
+++ showSESDemo.js
@@ -172,7 +172,12 @@ function showSESDemo() {
                     ctx.strokeRect(i*10, c.height-(soegle[DOEDE]+soegle[SYGE])*10, 10, soegle[SYGE]*10);
                 }
 
-                // Tegn de immune som sorte rect i y=døde+syge
+                // Tegn de immune som grøn rect i y=døde+syge
+                if (soegle[SYGE] != 0) {
+                    ctx.strokeStyle = "#008000"; //red
+                    ctx.strokeRect(i*10, c.height-(soegle[DOEDE]+soegle[SYGE]+soegle[IMMUNE])*10, 10, soegle[IMMUNE]*10);
+                }
+
                 // De raske er hvide, så der tegner vi ikke noget
             }
 
